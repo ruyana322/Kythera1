d1337d3574ee14571cc6e2e953464e25993db2b7
ruyana322 <156115962+ruyana322@users.noreply.github.com>
2026-07-08 11:15:18 +0700

    Set Kotlin 1.8 and Java 11 for subprojects
    Configured Kotlin and Java versions for all subprojects.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,16 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    // INI OBATNYA: Paksa semua plugin jadul buat naik kelas ke Kotlin 1.8 & Java 11
+    afterEvaluate {
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            kotlinOptions {
+                jvmTarget = "11"
+                languageVersion = "1.8"
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
